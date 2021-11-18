55d253d1b3c310e699b06eaa7e2c5c6629bd6e6d
yana682 <79636122+yana682@users.noreply.github.com>
2021-11-18 08:56:03 +0300

    Update text.js

--- text.js
+++ text.js
@@ -1,9 +1,14 @@
-const bitfont_alphabet = [
+//отрисовка пиксельного текста на главном канвасе
+
+//массив использованных буков и цифр
+const bitfontAlphabet = [
   "|", "%", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ",
-  "a", "c", "e", "g", "h", "i", "l", "m", "o", "r", "s", "t", "v"
+  "a", "e", "g", "i", "l", "m", "o", "r", "t", "v"
 ];
-const bitfont_characters = [
-  [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ], // |
+//отрисовка пикселями алфавита
+//3 столбца и 4 строки, 1 закрашенный пиксель, 0 незакрашенный
+const bitfontCharacters = [
+  [ 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0 ], // жизни
   [ 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1 ], // %
   [ 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1 ], // 0
   [ 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0 ], // 1
@@ -17,76 +22,72 @@ const bitfont_characters = [
   [ 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1 ], // 9
   [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ], // <space>
   [ 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1 ], // a
-  [ 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1 ], // c
   [ 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1 ], // e
   [ 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1 ], // g
-  [ 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1 ], // h
   [ 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0 ], // i
   [ 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1 ], // l
   [ 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1 ], // m
   [ 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1 ], // o
   [ 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1 ], // r
-  [ 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1 ], // s
   [ 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0 ], // t
   [ 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0 ]  // v
 ];
-const bitfont_kerning = [
+//изменение интервала между буквами, чтобы небыло наложения
+const bitfontKerning = [
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
-  0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0
+  0, 0, 0, 2, 0, 0, 0, 0, 0, 0,
 ];
-let BitmapFont = function(alphabet, characters, kerning, w, h, pixel_size) {
+//класс отрисовки и её параметры
+let BitmapFont = function(alphabet, characters, kerning, w, h, pixelSize) {
   this.alphabet = alphabet;     // Шрифт алфавита.
   this.characters = characters; // Символы шрифта.
   this.kerning = kerning;       // Кернинг.
   this.w = w;                   // Ширина шрифта.
   this.h = h;                   // Высота шрифта.
-  this.pixel_size = pixel_size;
+  this.pixelSize = pixelSize;
 
-  // Визуализировать одиночный символ в заданной позиции.
-  this.render_character = function(context, x, y, char, color) {
-    let alphabet_idx = this.alphabet.indexOf(char);
-    if (alphabet_idx < 0)
+  // Визуализируем символ в нужной позиции
+  this.renderCharacter = function(context, x, y, char, color) {
+    let alphabetIdx = this.alphabet.indexOf(char);
+    if (alphabetIdx < 0)
       return 0;
 
-    let bit_char = this.characters[alphabet_idx];
+    let bit = this.characters[alphabetIdx];
 
-    context.fillStyle = color;
-    for (let row = 0; row < this.h; ++row)
-      for (let col = 0; col < this.w; ++col)
-        if (bit_char[row * this.w + col])
-          context.fillRect(x + col * pixel_size, y + row * pixel_size,
-                           pixel_size, pixel_size);
+    context.fillStyle = color;  //закрашивание(происходит в другом скрипте)
+    for (let row = 0; row < this.h; ++row)   //для отрисовки рядов
+      for (let col = 0; col < this.w; ++col) //для отрисовки столбцов
+        if (bit[row * this.w + col])
+          context.fillRect(x + col * pixelSize, y + row * pixelSize, pixelSize, pixelSize);//заполнение
 
-    return this.w - this.kerning[alphabet_idx];
+    return this.w - this.kerning[alphabetIdx];
   };
-
-  //Визуализировать строку, начиная с заданной позиции.
-  this.render_string = function(context, x, y, string, color) {
-    let cur_x = x;
+  //визуализируем строку
+  this.renderString = function(context, x, y, string, color) {
+    let curX = x;
     let dx;
     for (let i = 0; i < string.length; ++i) {
-      dx = this.render_character(context, cur_x, y, string.charAt(i), color);
-      cur_x += (dx + 1) * this.pixel_size;
+      dx = this.renderCharacter(context, curX, y, string.charAt(i), color);
+      curX += (dx + 1) * this.pixelSize;
     }
   };
-
-  //Вычислить ширину символа без его рендеринга.
-  this.char_width = function(char) {
-    let alphabet_idx = this.alphabet.indexOf(char);
-    if (alphabet_idx < 0)
+  //вычисление ширины символа
+  this.charWidth = function(char) {
+    let alphabetIdx = this.alphabet.indexOf(char);
+    if (alphabetIdx < 0)
       return 0;
 
     else
-      return (this.w - this.kerning[alphabet_idx]) * this.pixel_size;
+      return (this.w - this.kerning[alphabetIdx]) * this.pixelSize;
   };
-
-  // Вычислить ширину строки без ее фактического рендеринга.
-  this.string_width = function(string) {
+  //вычисление ширины строки для дальнейшего размещения текста
+  this.stringWidth = function(string) {
     let w = 0;
     for (let i = 0; i < string.length; ++i)
-      w += this.char_width(string.charAt(i)) + this.pixel_size;
-    w -= this.pixel_size;
+      w += this.charWidth(string.charAt(i)) + this.pixelSize;
+    w -= this.pixelSize;
     return w;
   };
 };
-let bitfont = new BitmapFont(bitfont_alphabet, bitfont_characters, bitfont_kerning, 3, 5, tileSize);
+//последние 3 параметра это ширина шрифта в клтках, высота в клетках и размер клетки
+let bitfont = new BitmapFont(bitfontAlphabet, bitfontCharacters, bitfontKerning, 3, 5, 5);
